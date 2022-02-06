5fc47af6dee875dfdb9de6f27325736a1b144ad8
Christian Ortiz <christianortiz.3@outlook.com>
2022-02-05 20:47:38 -0600

    Set up webpack & barebones html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
-const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-
+  mode: 'development',
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(DIST_DIR),
+  },
+  module : {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ],
+  },
 };
