9f3acca092d4e964af1484e52ee456726743e43f
Joann Whang <joann.whang@gmail.com>
2022-02-08 23:03:01 -0600

    test: update script to remove watch, add eslint config and ignore files, and clean up code on test files

--- client/src/qa/QA-enzyme.test.jsx
+++ client/src/qa/QA-enzyme.test.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-configure({ adapter: new Adapter() });
 import QA from './QA';
 
+configure({ adapter: new Adapter() });
 
 test('QA component renders', () => {
   const heading = shallow(
