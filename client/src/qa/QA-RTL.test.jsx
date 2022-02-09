15fd34617f62b13054dd51c279b4ac4de7e8add1
Joann Whang <joann.whang@gmail.com>
2022-02-08 19:58:50 -0600

    test: add RTL and RTL/jest-dom as devDependencies with failing QA-RTL.test.jsx

---
+++ client/src/qa/QA-RTL.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+ 
+const Goodbye = () => {
+  return <h1>Bye Everyone</h1>;
+};
+ 
+test('should print the Goodbye component', () => {
+  render(<Goodbye/>);
+  screen.debug();
+});
