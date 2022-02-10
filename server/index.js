af4fb7dca0ad554d8f005d0af6945b32d277f6ee
Joann Whang <joann.whang@gmail.com>
2022-02-10 00:54:19 -0600

    qa routes in server to look at data

--- server/index.js
+++ server/index.js
@@ -8,12 +8,32 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+// Jo additions
+const config = require('../client/src/config/github.js');
 
+const axios = require('axios');
 
+app.all('/*', (req, res, next) => {
+  req.options = {
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${req.url}`,
+    headers: {
+      Authorization: config
+    }
+  };
 
+  next();
+});
 
+app.get('/qa/questions', (req, res, next) => {
+  console.log('req.options HERE', req.options);
 
+  axios(req.options)
+    .then((response) => {
+      console.log(response.data.results.length);
+      res.send(response.data);
+    });
 
+});
 
 
 
