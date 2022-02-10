972f2084723abb98ab84a0e9eadbee7d0af52da6
Joann Whang <joann.whang@gmail.com>
2022-02-10 02:33:05 -0600

    qa put route in server

--- server/index.js
+++ server/index.js
@@ -14,18 +14,21 @@ const config = require('../client/src/config/github.js');
 const axios = require('axios');
 
 app.all('/*', (req, res, next) => {
+  console.log('req method', req.method);
   req.options = {
+    method: req.method,
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${req.url}`,
     headers: {
       Authorization: config
     }
   };
+  console.log('req.options', req.options);
 
   next();
 });
 
-app.get('/qa/questions', (req, res, next) => {
-  console.log('req.options HERE', req.options);
+app.get('/qa/questions', (req, res) => {
+  console.log('GET HERE', req.options);
 
   axios(req.options)
     .then((response) => {
@@ -34,6 +37,20 @@ app.get('/qa/questions', (req, res, next) => {
     });
 
 });
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log('PUT HERE', req.options);
+  
+  axios(req.options)
+    .then((response) => {
+      console.log('PUT RESPONSE', response);
+      res.send('PUT SUCCESS');
+    });
+ 
+  
+});
+
+
 
 
 
