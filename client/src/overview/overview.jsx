6fe980c6091c84e2b8f38767d95e78b244aafe42
ChloeM <meinshac@gmail.com>
2022-02-10 23:31:18 -0500

    create initial overview app

---
+++ client/src/overview/overview.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h3>Overview</h3>
+    );
+  }
+}
+
+export default Overview;
