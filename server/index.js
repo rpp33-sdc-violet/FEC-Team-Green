9a355e767b4df8c64385172435e97c31058ec61a
Joann Whang <joann.whang@gmail.com>
2022-02-10 23:17:15 -0600

    feat: wildcard middleware to attach method, url, and authentication header

--- server/index.js
+++ server/index.js
@@ -8,12 +8,13 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-// Jo additions
+// FROM G-LEARN: Instead consider setting up a wildcard route, "/*" which is capable of capturing the req.method and req.url from the incoming request, attaching the Authentication header, and making the same request to the API server, before capturing the response data and sending it back to the client.
 const config = require('../client/src/config/github.js');
-const axios = require('axios');
+// I used axios to test out some API calls but we can eventually move this the controller files.
+const axios = require('axios'); 
 
+// WILDCARD ROUTE: using app.all (found it in the express docs: https://expressjs.com/en/guide/routing.html)
 app.all('/*', (req, res, next) => {
-  console.log('req method', req.method);
   req.options = {
     method: req.method,
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${req.url}`,
@@ -26,22 +27,21 @@ app.all('/*', (req, res, next) => {
   next();
 });
 
+// SOME EXAMPLE CALLS TO THE API -- I WILL EVENTUALLY PUT THESE INSIDE A SEPARATE FILE FOR QA RELATED CALLS TO THE API
 app.get('/qa/questions', (req, res) => {
-  console.log('GET HERE', req.options);
+  console.log('QA GET HERE', req.options);
 
   axios(req.options)
     .then((response) => {
-      console.log(response.data.results.length);
       res.send(response.data);
     });
 });
 
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  console.log('PUT HERE', req.options);
+  console.log('QA PUT HERE', req.options);
   
   axios(req.options)
     .then((response) => {
-      console.log('PUT RESPONSE', response);
       res.send('PUT SUCCESS');
     });
 });
