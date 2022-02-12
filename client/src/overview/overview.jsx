50ffcc720cdd3dc37449a60dfe34b935e7a4c5cf
ChloeM <meinshac@gmail.com>
2022-02-12 01:01:19 -0500

    refactor overview

--- client/src/overview/overview.jsx
+++ client/src/overview/overview.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, UseEffect } from 'react';
 
-class Overview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Overview = (props) => {
 
-  render() {
-    return (
-      <h3>Overview</h3>
-    );
-  }
-}
+  //const [urlList, setUrlList] = useState([]);
+  //const [thumbUrlList, setThumbUrlList] = useState([]);
+
+
+  const [styleList, setStyleList] = useState([]);
+  // styleList will contain an array of styles for a specific product
+
+  const [productOverview, setProductOverview] = useState({});
+  // product overview will contain
+  //  id, name, slogan, description, category, default_price, features (an array)
+
+  const [selectedStyle, setSelectedStyle] = useState('');
+  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedQuantity, setSelectedQuantity] = useState('');
+
+
+  useEffect(()=> {
+
+  });
+
+  return (
+    <h3>Overview</h3>
+  );
+
+};
 
 export default Overview;
