282999382ff1b3e96fc5b7c5ff409c58b1387d8a
ChloeM <meinshac@gmail.com>
2022-02-12 01:06:03 -0500

    wip: create styleSelector

---
+++ client/src/overview/components/StyleSelector.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+
+const StyleSelector = (props) => {
+
+  //props
+  //styleList;
+  //setSelectedImageUrl
+  //setSelectedThumbnailUrl
+
+
+  return (
+    <div className ='style-selector'>
+      StyleSelector
+    </div>
+  );
+};
+
+export default StyleSelector;
