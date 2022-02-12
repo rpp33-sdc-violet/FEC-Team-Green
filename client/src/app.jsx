3e98cb9e7b026cc3a7208839887290f12b813d1d
Christian Ortiz <christianortiz.3@outlook.com>
2022-02-12 15:18:38 -0600

    Made initial RelatedProducts component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Overview from './overview/Overview.jsx';
+import RelatedProducts from './relatedProducts/RelatedProducts.jsx';
 import QA from './qa/QA.jsx';
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
       <div>
         <h1>TeamGreen!</h1>
         <Overview></Overview>
+        <RelatedProducts></RelatedProducts>
         <QA></QA>
       </div>
     );
