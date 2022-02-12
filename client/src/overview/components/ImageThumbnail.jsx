a4ac86ca01b7557c9544252b736d8fad26e18d19
ChloeM <meinshac@gmail.com>
2022-02-11 23:15:02 -0500

    create ImageThubmnail skeleton

---
+++ client/src/overview/components/ImageThumbnail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+var ImageThumbnail = function (props) {
+
+  //props props.imageNav;
+
+
+  return (
+    <div className ='img-thumbnail'>
+      ImageThumbnail
+    </div>
+  );
+};
+
+export default ImageThumbnail;
+
+//Test does clicking the thumbnail expand the image
+//Test does clicking the thumbnail bring that thumbnail to the top of the list.
+//is the Thumbnail part of a ImgThumbnailList
