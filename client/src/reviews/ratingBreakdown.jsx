cc0ce00d3ee019f426b98aa98d353414b76234ae
Wendyddw <arielddw@gmail.com>
2022-02-12 21:32:12 -0800

    added unsaved files

--- client/src/reviews/ratingBreakdown.jsx
+++ client/src/reviews/ratingBreakdown.jsx
@@ -1,1 +1,5 @@
 //show and handle the rating bars
+
+//special funciton: clicking the breakdown for a star count will filter the review list
+//may need to create a seprate helper component, in order to communicate with and filter review list
+//it will be passed to widget top level component
