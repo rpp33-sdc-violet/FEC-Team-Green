a83bdb11a1f94946d537e9d0dcc126545b57f7ed
ChloeM <meinshac@gmail.com>
2022-02-13 01:01:58 -0500

    chore: complete skeleton with some test ideas

---
+++ client/src/overview/components/SelectQuantityDropdown.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+// import StyleList from './StyleList.jsx';
+
+//this is the container for my quantity selector
+const SelectQuantityDropdown = (props) => {
+
+  //props
+  //selectedStyleQuantity
+  //setSelectedQuantity()
+
+
+
+  return (
+    <div className ='select-quantity'>
+      SelectQuantityDropdown
+    </div>
+  );
+};
+
+export default SelectQuantityDropdown;
+
+//allows user to select a quantity from a list.
+// from 1 to either quantity or 15, which ever is smaller
+
+/*
+Test if I click the button will a dropdown menu appear.
+Test if numbers in list stop when reaching 15 when quantitiy is larger.
+Test if numbers in list stop before reaching 15 when quantitiy is smaller than 15
+
+*/
