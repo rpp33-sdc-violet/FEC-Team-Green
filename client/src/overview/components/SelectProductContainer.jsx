f1173f6c788e0ede4429ca4f83b0fef28d2eeb6b
ChloeM <meinshac@gmail.com>
2022-02-12 22:55:31 -0500

    created skeleton of SelectProductContainer

---
+++ client/src/overview/components/SelectProductContainer.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import StyleList from './StyleList.jsx';
+
+//this is the container for all parts of the style functionality
+const SelectProductContainer = (props) => {
+
+  //props
+  //styleList;
+  //setSelectedImageUrl
+  //setSelectedThumbnailUrl
+  //
+
+
+  return (
+    <div className ='select-product'>
+      SelectProductContainer
+    </div>
+  );
+};
+
+export default SelectProductContainer;
+
+//displays the selected style
+// a list of styles to select from
+// the selcted style will also have a checkmark on it
+// the styles will be displayed in a list
