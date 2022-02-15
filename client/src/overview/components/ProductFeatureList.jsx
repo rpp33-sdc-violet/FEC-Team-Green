47b00dd7768e291fbd7a9e69e6f6be1b12329558
ChloeM <meinshac@gmail.com>
2022-02-15 02:00:46 -0500

    moved style file

---
+++ client/src/overview/components/ProductFeatureList.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+var pFeatureList = {
+  'display': 'grid',
+  'backgroundColor': 'pink',
+
+  'grid-template-columns': '1fr',
+  'grid-template-rows': 'minmax(100px, auto)',
+};
+const ProductFeatureList = (props)=> {
+
+  //props
+
+  //
+
+
+
+
+  return (
+    <div  style={pFeatureList}>
+
+      <div className='feature-list'> FeatureList </div>
+    </div>
+
+  );
+};
+
+export default ProductFeatureList;
+
+/*
+ TODO
+*/
