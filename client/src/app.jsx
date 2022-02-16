f4b0ae3e873d64adcca75de1e9dd7192fdabc350
ChloeM <meinshac@gmail.com>
2022-02-16 00:42:37 -0500

    add fake data source to app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -4,7 +4,7 @@ import Overview from './overview/Overview.jsx';
 import RelatedProducts from './relatedProducts/RelatedProducts.jsx';
 import QA from './qa/QA.jsx';
 import ReviewList from './reviews/reviewList.jsx';
-
+import exampleProductData from './data/exampleProductData.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,7 +14,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello TeamGreen Test</h1>
-        <Overview></Overview>
+        <Overview data={exampleProductData[0]}></Overview>
         <RelatedProducts data={{productID: '007'}}></RelatedProducts>
         <QA product_id={64624}></QA>
       </div>
