724ce700307e0bb6948ae7b3e0e56d7107a68bd5
Joann Whang <joann.whang@gmail.com>
2022-02-16 20:44:12 -0600

    useEffect to get all answers in IndividualQuestion component

--- client/src/qa/components/IndividualQuestion.jsx
+++ client/src/qa/components/IndividualQuestion.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import AddAnswerLink from './AddAnswerLink.jsx';
 import AnswersList from './AnswersList.jsx';
+
 
 const IndividualQuestion = (props) => {
   // props
@@ -15,6 +17,42 @@ const IndividualQuestion = (props) => {
 
   // methods
   // getAllAnswers/useEffect - update answers and helpfulCount, invoke setMoreAnsButtonVisible
+  useEffect(() => {
+    const count = 20; // start with a high number to avoid too many calls to the API
+    let page = 1;
+    let answers = [];
+    // inner function that keeps calling until length of data is less than the count
+    const getAllAnswers = () => {
+      axios.get(`/api/qa/questions/${props.question.question_id}/answers?count=${count}&page=${page}`)
+        .then((response) => {
+          answers = answers.concat(response.data.results);
+          if (response.data.results.length === count) { // there are still more answers
+            page++; // increment page to get more answers
+            getAllAnswers();
+          } else { // length is less than count so all answers have been retrieved
+            // loop over all answers
+            for (let i = 0; i < answers.length; i++) {
+              // if the current element's answerer_name is Seller
+              if (answers[i].answerer_name === 'Seller') {
+                // save seller answer data
+                let seller = answers[i];
+                // remove from current position
+                answers.splice(i, 1);
+                // unshift to front of array
+                answers.unshift(seller);
+              }
+            }
+            setAnswers(answers);
+            return;
+          }
+        })
+        .catch((error) => {
+          console.log('ERROR IN getAllAnswers', error);
+        });
+    };
+    // invoke the recursive inner function
+    getAllAnswers();
+  }, []); // the empty array stops the effect from running more than once
 
   // isMoreAnsButtonVisible - checks countA with answers length, if countA less than length -> true, else, false
 
