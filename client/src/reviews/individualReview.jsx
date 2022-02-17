6be378beccdb093a777cf622aa2f05008e9c1c8c
Wendyddw <arielddw@gmail.com>
2022-02-17 14:30:06 -0800

    completed date of review, review summary, review body text

--- client/src/reviews/individualReview.jsx
+++ client/src/reviews/individualReview.jsx
@@ -6,12 +6,46 @@ class IndividualReview extends React.Component {
     this.state = {
 
     };
+    this.moreReview = this.moreReview.bind(this);
   }
+
+  moreReview(reviewId, fullReview) {
+    //Change the HTML content of a <p> element with id="x":
+    document.getElementById(reviewId).innerHTML = fullReview;
+  }
+
   render() {
+    let review = this.props.review;
+    let summary = review.summary;
+    let fullReview = null;
+    let reviewId = review.review_id;
+    if (summary.length > 60) {
+      summary = summary.substring(0, 60) + '...';
+    }
+
+    let reviewBody = review.body;
+
+    //let reviewBody = 'long reivew test long reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew testlong reivew test';
+    if (reviewBody.length > 250) {
+      reviewBody = reviewBody.substring(0, 250);
+      fullReview = <div>
+        <div className ='reviewBody' id = {reviewId}>{reviewBody}</div>
+        <a href='#' onClick = {() => { this.moreReview(reviewId, review.body); }}>Show more</a>
+      </div>;
+    } else {
+      fullReview = <div>{reviewBody}</div>;
+    }
+
     return (
       <div>
-        {this.props.review.summary}
+        <div className='review'>{new Date(this.props.review.date).toLocaleString('en-US', {month: 'long', day: '2-digit', year: 'numeric'})}</div>
+        <div>
+          {summary}
+        </div>
+        <div>{fullReview}</div>
+        <br></br>
       </div>
+
     );
   }
 
