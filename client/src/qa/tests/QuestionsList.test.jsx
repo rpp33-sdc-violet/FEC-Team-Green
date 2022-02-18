e8aeb3acbc1866f4f126ed6436c7d7bea374a4a5
Joann Whang <joann.whang@gmail.com>
2022-02-18 17:14:18 -0600

    test: complete QuestionsList tests

---
+++ client/src/qa/tests/QuestionsList.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import QuestionsList from '../components/QuestionsList.jsx';
+// fixture
+import { exampleQuestionData64624, exampleQuestionData64621 } from '../../data/exampleQuestionData.js';
+// 64624 = 5, 64621 = 8
+
+describe('QUESTIONS LIST', () => {
+  it('should render five qestions if five questions are passed as props', () => {
+    render(<QuestionsList questions={exampleQuestionData64624.results} />);
+    const questionsList = screen.getByRole('questions-list');
+    const { getAllByRole } = within(questionsList);
+    const questions = getAllByRole('question');
+    expect(questions.length).toBe(5);
+  });
+
+  it('should render eight qestions if eight questions are passed as props', () => {
+    render(<QuestionsList questions={exampleQuestionData64621.results} />);
+    const questionsList = screen.getByRole('questions-list');
+    const { getAllByRole } = within(questionsList);
+    const questions = getAllByRole('question');
+    expect(questions.length).toBe(8);
+  });
+});
