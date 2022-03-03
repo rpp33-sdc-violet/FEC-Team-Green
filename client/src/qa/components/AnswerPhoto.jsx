c5aa2b7ae4b9b2462469e83693fc2eee2bcb74b9
Joann Whang <joann.whang@gmail.com>
2022-03-02 22:18:46 -0600

    feat: add modal to expand thumbnail to full resolution image

--- client/src/qa/components/AnswerPhoto.jsx
+++ client/src/qa/components/AnswerPhoto.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal.jsx';
 
 const AnswerPhoto = (props) => {
+  const [show, setShow] = useState(false);
+
+  const showModal = () => {
+    setShow(true);
+  };
+
+  const hideModal = () => {
+    setShow(false);
+  };
+
+
   return (
-    <img className="answer-photo-thumbnail"
-      src={props.url}
-      alt={`photo upload by ${props.answerer_name}`}></img>
+    <>
+      <Modal show={show} handleClose={hideModal}>
+        <img className="answer-photo"
+          src={props.url}
+          alt={`photo upload by ${props.answerer_name}`} />
+      </Modal>
+      <img className="answer-photo-thumbnail"
+        src={props.url}
+        alt={`photo upload by ${props.answerer_name}`}
+        onClick={showModal} />
+    </>
   );
 };
 
