6791baa413c6379c9f4b854aee2efeddafc53306
Joann Whang <joann.whang@gmail.com>
2022-03-09 22:50:58 -0600

    use React.lazy on QA child components

---
+++ client/dist/166.bundle.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfec_team_green=self.webpackChunkfec_team_green||[]).push([[166],{6166:(e,a,r)=>{r.r(a),r.d(a,{default:()=>t});var c=r(7294),n=r(7516);const t=function(e){return c.createElement("form",{role:"search",className:"search"},c.createElement("input",{type:"search",id:"question-search",name:"q",placeholder:"HAVE A QUESTION? SEARCH FOR ANSWERS...","aria-label":"Search questions",onChange:function(a){e.userSearch(a.target.value)}}),c.createElement(n.wnI,{id:"QA-search-icon"}))}}}]);
+//# sourceMappingURL=166.bundle.js.map
