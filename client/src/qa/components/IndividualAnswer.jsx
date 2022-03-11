1cfb130f441a56425cbf41dd00ed4ee5ccc79be2
Joann Whang <joann.whang@gmail.com>
2022-03-10 22:50:48 -0600

    fix: formatDate

--- client/src/qa/components/IndividualAnswer.jsx
+++ client/src/qa/components/IndividualAnswer.jsx
@@ -62,7 +62,7 @@ const IndividualAnswer = (props) => {
     <div className="answer" role="answer">
       <p className="answer-text"><span id="answer-letter">A:&nbsp;&nbsp;</span>{props.answer.body}</p>
       <AnswerPhotosList photos={props.answer.photos} answerer_name={props.answer.answerer_name} />
-      <footer className="answerData-helpfulA-reportLink">by {props.answer.answerer_name}, {formatDate()}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Helpful? <a href="/" onClick={handleHelpfulClick}>Yes</a> ({helpfulCountA})&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="/" onClick={handleReportClick}>{reportText}</a></footer>
+      <footer className="answerData-helpfulA-reportLink">by {props.answer.answerer_name}, {formatDate(props.answer.date)}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Helpful? <a href="/" onClick={handleHelpfulClick}>Yes</a> ({helpfulCountA})&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="/" onClick={handleReportClick}>{reportText}</a></footer>
     </div>
   );
 };
