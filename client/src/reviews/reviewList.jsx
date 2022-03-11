87ed6c229e29dcfb65b0ac44b3e356738e313291
Wendyddw <arielddw@gmail.com>
2022-03-10 23:25:52 -0800

    Deleted example data  Please enter the commit message for your changes. Lines starting

--- client/src/reviews/reviewList.jsx
+++ client/src/reviews/reviewList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import reviewSample from './exampleData.js';
 import IndividualReview from './individualReview.jsx';
 import AddNewReview from './addNewReview.jsx';
 import Ratings from './ratings.jsx';
