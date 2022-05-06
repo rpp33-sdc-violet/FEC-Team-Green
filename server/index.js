9fb66ef7f6a15652f9b4175e830dd5299774ff53
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-05-06 09:57:56 +0700

    Neen's change

--- server/index.js
+++ server/index.js
@@ -81,11 +81,11 @@ app.all('*', function (req, res, next) {
 });
 
 // const config = require('../client/src/config/github.js'); <-- REPLACED WITH DOTENV CONFIG
-require('dotenv').config({path: './server/.env'}); // <-- retrieve Github API Key in our .env file
+require('dotenv').config({path: './env'}); // <-- retrieve Github API Key in our .env file
 const GITHUB_API_KEY = process.env.GITHUB_API_KEY;
 // when you send a request to the '/api/**' endpoint, it automatically re-routed to the API server(done by pathRewrite)
 const options = {
-  target: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp', //target host
+  target: 'http://localhost:8080/', //target host
   changeOrigin: 'true', //needed for virtual hosted sites
   headers: {
     'Authorization': GITHUB_API_KEY
